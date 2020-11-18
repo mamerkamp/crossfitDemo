f9a5134c93e813df1ed52995a2bac3c595e2f298
Mark Amerkamp <markamerkamp@gmail.com>
2020-11-18 11:42:15 +0100

    Initial commit

---
+++ src/main/java/nl/amerkamp/java/crossfit/crossfitDemo/CrossfitDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.amerkamp.java.crossfit.crossfitDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/amerkamp/java/crossfit/crossfitDemo/CrossfitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.amerkamp.java.crossfit.crossfitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
